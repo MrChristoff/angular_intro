dc7051171b06fec4767ca785637575225d14e4fd
Chris Lawrence <chris.lawrence@lexisnexis.co.uk>
2018-12-04 14:56:34 +0000

    Starter files with annotation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
+// Import { Member name } from 'path to member'
 import { Component } from '@angular/core';
 
+// Metadata
 @Component({
+  // the name of the 'Directive' (custom HTML tag) to place the component  
   selector: 'pm-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+// Class
+// Naming convention 'FeatureName' suffixed with 'Component'
+// conventional name for root application component is 'AppComponent'
 export class AppComponent {
+  // Property 
   title = 'Angular: Getting Started';
 }
